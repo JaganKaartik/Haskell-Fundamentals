c87840498e997b842f5eaca205c8b3dd2799b7d0
JaganKaartik <kaartikjagan@gmail.com>
2019-11-01 10:35:49 +0530

    Day1.hs Added

---
+++ Haskell-Fundamentals/day1.hs
@@ -0,0 +1,25 @@
+-- Take While Using Foldr
+
+myTakeWhile' f xs = foldl (\rs x -> if f x then x:rs else []) [] (reverse xs)
+
+-- Palindrome
+
+palindrome :: Eq a => [a] -> Bool
+palindrome []  = True
+palindrome [_] = True
+palindrome xs  = (head xs) == (last xs) && (palindrome $ init $ tail xs)
+
+-- Map using Foldr
+
+map'            :: (a -> b) -> [a] -> [b]
+map' f []       = []
+map' f (x:xs)   = foldr (\y ys -> (f y):ys) [] xs
+
+-- Filter using Foldr
+
+filter' p = foldr (\x xs -> if p x then x : xs else xs) []
+
+-- Filter using if-else
+fil::(a->Bool)->[a]->[a]
+fil p [] = []
+fil p (x:xs) = if p x then x : fil p xs else fil p xs
