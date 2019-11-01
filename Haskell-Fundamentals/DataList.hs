b2b3990d7942ef23112ddfe3e1917664ef1b5748
JaganKaartik <kaartikjagan@gmail.com>
2019-11-01 17:26:09 +0530

    Union, Intersect, Insert

--- Haskell-Fundamentals/DataList.hs
+++ Haskell-Fundamentals/DataList.hs
@@ -1,5 +1,20 @@
--- Element Indices
-' ' `elemIndices` "Where are the spaces?"
+import Data.List
 
 -- Element Indices
-[12] `elemIndices` [1..50]
+elemi2 = ' ' `elemIndices` "Where are the spaces?"
+
+-- Element Indices
+elemi1 = [12] `elemIndices` [1..50]
+
+-- Union
+
+u = [1,2,3] `union` [4,5,6]
+
+-- Intersect
+
+ui = [1,2,3,4,5] `intersect` [4,5,6]
+
+-- insert
+
+insert 4 [3,5,1,2,8,2] 
+insert (-4) [3,5,1,2,8,2] 
