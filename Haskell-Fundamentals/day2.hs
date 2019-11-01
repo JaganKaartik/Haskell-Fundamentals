76e87572e7c6c203c2726205b9ba4bf949723bd1
JaganKaartik <kaartikjagan@gmail.com>
2019-11-01 15:04:38 +0530

    Updates

--- Haskell-Fundamentals/day2.hs
+++ Haskell-Fundamentals/day2.hs
@@ -6,3 +6,18 @@ dort = map (+4).filter even
 -- 2nd Function Decomposition example
 
 dfunc = sum.map (+4).filter even
+
+-- 3rd Function Decomposition example
+
+sumcubes = sum $ takeWhile (<10000) $ map (^3) [1..] 
+
+-- Where to use . and $
+
+-- take all cubes under the limit of  1000 [1,8,27,64,125,216,343,512,729]
+
+-- takeWhile (<1000) of cubes map (^3)
+
+cubesFunc  = takeWhile (<1000) . map (^3)
+
+cubesWFunc = takeWhile (<1000) $ map (^3) [1..] 
+
