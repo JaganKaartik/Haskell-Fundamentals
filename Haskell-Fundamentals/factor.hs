f221adc88a40abf8225bda38e34e0b4cd8ed61f4
JaganKaartik <kaartikjagan@gmail.com>
2019-11-02 23:50:03 +0530

    Updated Files

--- Haskell-Fundamentals/factor.hs
+++ Haskell-Fundamentals/factor.hs
@@ -1,2 +1,4 @@
+-- To find the factor of a number
+
 factor::Int -> [Int]
 factor n = [x | x<-[1..n], n `mod` x == 0 ]
--- Haskell-Fundamentals/string_count.hs
+++ Haskell-Fundamentals/string_count.hs
@@ -1,2 +1,4 @@
+-- Count no of occurences of x in xs
+
 strcount::Char -> [Char] -> Int
 strcount x xs = length [ x' | x'<-xs, x==x' ]
